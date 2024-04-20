5e1ce15e35456748f988ae29976dcf9fdc115f66
Karthick Ganesan <karthick.ganesh@outlook.com>
2024-04-20 16:26:33 +0530

    Fetching Genres using react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import App from './App'
-import theme from './theme'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import App from "./App";
+import theme from "./theme";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      //retry: 3,
+      // cacheTime: 300_000, //5 min
+      // staleTime: 10 * 1000, //10S
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
