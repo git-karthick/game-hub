27eed42834327483fe00aeb4eeb884783bd66240
Karthick Ganesan <karthick.ganesh@outlook.com>
2024-04-28 12:55:36 +0530

    Build Expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,7 @@
 import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
@@ -8,12 +9,12 @@ const GameDetailPage = () => {
   const { data: game, isLoading, error } = useGame(slug!);
   if (isLoading) return <Spinner />;
 
-  if (error) throw error;
+  if (error || !game) throw error;
   return (
     <>
       <Box padding={5}>
-        <Heading>{game?.name}</Heading>
-        <Text>{game?.description_raw}</Text>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
       </Box>
     </>
   );
